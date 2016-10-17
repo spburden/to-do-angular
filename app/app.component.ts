259d2b27d189842f8389e2d12e3e5f27e7f82fa4
Matthew Brandenburg and Stephen Burden <matthew+stephen@>
2016-10-17 11:48:08 -0700

    add everything until Using Built-In Event Emitters

---
+++ app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>My First Angular 2 App</h1>
+    <h3 *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+  </div>
+  `
+})
+
+export class AppComponent {
+  public tasks: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+  ];
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {   }
+}
